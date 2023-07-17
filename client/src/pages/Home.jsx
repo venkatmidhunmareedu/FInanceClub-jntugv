6ee6ad948383da47d346770eaf39f6a446f737f0
M V Midhun <midhunmareedu@gmail.com>
2023-07-17 20:18:50 +0530

    made filter menu UI

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -33,23 +33,50 @@ const Home = () => {
             {isLoaded ? "" : <PageLoader />}
             <div className={isLoaded ? `` : `d-none`}>
                 <Navbar />
-
                 <div className="container">
                     <div className="row ">
-                        <span className="text-start">
-                            <i class="fa-solid fa-filter"></i>
-                        </span>
-                        <p className="babus fw-bolder text-lg-spacing fs-1 lt-sp-1 lt-sz-1 text-center">GV<span className="color-1">PEOPLE BLOGS</span></p>
-                        <div className="row">
-                            <div className="mb-4">
-                                <div className="row d-flex justify-content-center">
-                                    <HorizontalScrollbar>
-                                        {blogs.map((blog) => {
-                                            return <span class="btn open-sans badge bg-secondary text-white mx-1 mt-1">Web development</span>
-                                        })}
-                                    </HorizontalScrollbar>
+                        <p className="babus fw-bolder text-lg-spacing fs-1 lt-sp-1 lt-sz-1 text-center mb-5">GV<span className="color-1">PEOPLE BLOGS</span></p>
+                        <div className="col-sm-3 col-md-3 col-lg-3 mb-4">
+                            <p className="babus fs-4 text-center fw-bolder lt-sp-1 color-1">Filters</p>
+                            <div class="accordion" id="accordionExample">
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header">
+                                        <button class="accordion-button open-sans fw-bolder" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                            Category
+                                        </button>
+                                    </h2>
+                                    <div id="collapseOne" class="accordion-collapse collapse show open-sans " data-bs-parent="#accordionExample">
+                                        <div class="accordion-body">
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="checkbox" value="" />
+                                                <label class="form-check-label">
+                                                    Artificial intelligence and Machine learning
+                                                </label>
+                                            </div>
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="checkbox" value="" />
+                                                <label class="form-check-label">
+                                                    Web development
+                                                </label>
+                                            </div>
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="checkbox" value="" />
+                                                <label class="form-check-label">
+                                                    Block Chain
+                                                </label>
+                                            </div>
+                                        </div>
+                                        <div className="text-center">
+                                            <button class="filter-btn open-sans fw-bold"><i class="bi bi-filter"></i>Filter
+                                            </button>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
+                        </div>
+                        <div className="col-sm-9">
+
+
                             <div className="container-fluid">
                                 <div className="row d-flex justify-content-start">
                                     {blogs.map((blog) => {
