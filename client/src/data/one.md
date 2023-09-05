daf9e4568769c1307bef75ecbdf9717196f2bd1d
M V Midhun <midhunmareedu@gmail.com>
2023-09-05 22:23:56 +0530

    edited blogcomps

---
+++ client/src/data/one.md
@@ -0,0 +1,26 @@
+#Markdown Cheat Sheet
+A quick reference to the Markdown syntax.
+<br>
+##Overview
+This Markdown cheat sheet provides a quick overview of all the Markdown syntax elements. It can’t cover every edge case, so if you need more information about any of these elements, refer to the reference guides for basic syntax and extended syntax.
+<br>
+##Basic Syntax
+These are the elements outlined in John Gruber’s original design document. All Markdown applications support these elements.
+
+| Element | Markdown Syntax |
+| ----------- | ----------- |
+| Heading | # H1 <br> ## H2 <br> ### H3 |
+| Bold | ``` **bold text** ```|
+|Italic | ``` *italicized text* ```|
+|Blockquote | ``` > blockquote ```|
+|Ordered List| 1. First item <br>2. Second item <br>3. Third item|
+|Unordered List| - First item <br> - Second item <br> - Third item|
+|Code| ``` `code`  ```|
+|Horizontal Rule| ---|
+|Link| ``` [title] (https://www.example.com)```|
+|Image| ``` ![alt text](image.jpg) ```|
+<br>
+##Extended Syntax
+<br>
+These elements extend the basic syntax by adding additional features. Not all Markdown applications support these elements.
+
--- client/src/pages/BlogEdit.jsx
+++ client/src/pages/BlogEdit.jsx
@@ -4,6 +4,7 @@ import Footer from '../components/Footer'
 import { useNavigate } from 'react-router-dom'
 import { useTimer } from 'react-timer-hook';
 import Markdown from 'markdown-to-jsx'
+import { Table, Para, heading1, heading2, quote, code, image, list, olist } from "../components/BlogComps";
 
 
 
@@ -42,14 +43,14 @@ const Mytimer = ({ expiryTimestamp }) => {
 
 
 
-const save = (text) => {
+const save = (title, blogData) => {
 
 }
 
 
 const BlogEdit = () => {
     const [blogData, setData] = useState("");
-
+    const [title, setTitle] = useState("");
     const handleChange = (e) => {
         setData(e.target.value)
     }
@@ -58,12 +59,17 @@ const BlogEdit = () => {
         return (
             <div className='open-sans'>
                 <Navbar />
-                <p className='text-center fw-bolder'>To know more about Markdown Syntax visit <a href="https://www.markdownguide.org/basic-syntax/">https://www.markdownguide.org/basic-syntax/</a></p>
-                <div className='container text-start'>
-                    <button className='btn btn-outline-primary'>Save</button>
-                </div>
+                <p className='text-center fw-bolder'>To know more about Markdown Syntax visit <a target='_blank' href="https://www.markdownguide.org/cheat-sheet/">https://www.markdownguide.org/cheat-sheet/</a>
+                    <div className='container text-start '>
+                        <button type='submit' className='btn btn-outline-primary mb-3'>Save</button>
+                        <div class="form-floating mb-3 ">
+                            <input type="text" class="form-control" id="floatingInput" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="name@example.com" required />
+                            <label for="floatingInput">Title</label>
+                        </div>
+                    </div></p>
+
                 <div className='container d-flex justify-content-center vh-100 vw-100 my-2'>
-                    <div className='col-md-6 col-lg-6 col-xl-6 border mx-1'>
+                    <div className='col-md-5 col-lg-5 col-xl-5 border mx-1'>
                         <div className=''>
                             <div className='border-bottom text-center fw-bolder bg-white color-1'>Editor</div>
                             <div className='ms-1'>
@@ -71,18 +77,30 @@ const BlogEdit = () => {
                             </div>
                         </div>
                     </div>
-                    <div className='col-md-6 col-lg-6 col-xl-6 border mx-1'>
+                    <div className='col-md-7 col-lg-7 col-xl-7 border mx-1'>
                         <div className=''>
                             <div className='border-bottom text-center fw-bolder bg-color-1 text-white'>Preview</div>
-                            <div className='ms-1 my-1 scrollable col-12 ' style={{ height: "685px" }}>
-                                <Markdown >{blogData}
+                            <div className='ms-1 my-1 scrollable col-12 open-sans ' style={{ height: "685px" }}>
+                                <Markdown className="markdown" options={{
+                                    overrides: {
+                                        table: Table,
+                                        p: Para,
+                                        h1: heading1,
+                                        h2: heading2,
+                                        blockquote: quote,
+                                        code: code,
+                                        img : image,
+                                        ul : list,
+                                        ol : olist
+                                    }
+                                }} >{blogData}
                                 </Markdown>
                             </div>
                         </div>
                     </div>
 
                 </div>
-                
+
                 <Footer />
             </div>
         )
